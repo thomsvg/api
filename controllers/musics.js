b87d161535762a1d74d54f6b6d5f44b54c9f0441
thomsvg <thomas.sauvage6203@gmail.com>
2024-05-03 10:39:48 +0200

    Ajout Delete

--- controllers/musics.js
+++ controllers/musics.js
@@ -1,42 +1,52 @@
 //const data = require("./../models/data.json");
 const Music = require("../models/Musics");
-const controllerMusic = 
+const controllerMusic =
 {
-    find: async (req, res) =>{
-        if(req.query.search){
-                const query = req.query.search;
-                const result = data.filter(song => song.title.toLowerCase().includes(query.toLowerCase()));
-                if (result.length === 0) {
-                    return res.status(404).json({ error: "No matching songs found." });
-                }
-                res.status(200).json({ result });
-                console.log(result);
-        }else{
+    find: async (req, res) => {
+        if (req.query.search) {
+            const query = req.query.search;
+            const result = data.filter(song => song.title.toLowerCase().includes(query.toLowerCase()));
+            if (result.length === 0) {
+                return res.status(404).json({ error: "No matching songs found." });
+            }
+            res.status(200).json({ result });
+            console.log(result);
+        } else {
             const data = await Music.findAll();
-            res.status(200).json({result: data });
+            res.status(200).json({ result: data });
         }
     },
 
-create : async (req,res)=>{
-    console.log(req.body);
-    const data = await Music.create(req.body);
-    console.log(data);
-    res.status(201).json({message : "votre musique a été ajoutée",data : req.body});
-},
+    create: async (req, res) => {
+        console.log(req.body);
+        const data = await Music.create(req.body);
+        console.log(data);
+        res.status(201).json({ message: "votre musique a été ajoutée", data: req.body });
+    },
 
     findById: (req, res) => {
-        const id = req.params.id
-        if(isNaN(id)){
-            return res.status(400).json({error : "Don't use text for id"});
+        const id = req.params.id;
+        if (isNaN(id)) {
+            return res.status(400).json({ error: "Don't use text for id" });
         }
-        if(id > data.length){
-            return res.status(404).json({error : "ID not found ..."});
+        if (id > data.length) {
+            return res.status(404).json({ error: "ID not found ..." });
         }
 
-        return res.status(200).json({result : data[id-1]})
+        return res.status(200).json({ result: data[id - 1] });
     },
 
-    random: (req, res)=>{
+    delete: async (req, res) => {
+        const idMusic = req.params.id;
+        const data = await Music.destroy({
+            where :{
+                id:idMusic
+            }
+        });
+        res.status(200).json({message: "Suppression effectuée."})
+    },
+
+    random: (req, res) => {
 
     }
 }
